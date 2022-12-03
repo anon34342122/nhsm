42baf306eb6be4df300db93ee5497b2f06046c2d
anon34342122 <119206798+anon34342122@users.noreply.github.com>
2022-12-03 18:15:15 +0100

    Ensure bootStrap is only applied once - Add secondary log4j2 xml for tests

--- src/test/java/com/anon/nhsm/Bootstrap.java
+++ src/test/java/com/anon/nhsm/Bootstrap.java
@@ -8,15 +8,23 @@ import java.io.IOException;
 
 public class Bootstrap {
     static Logger LOGGER;
+    static boolean initialized;
 
     public static void bootStrap() {
+        if (initialized) {
+            return;
+        }
+
+        System.setProperty("log4j.configurationFile", "log4j2-test.xml");
         LOGGER = LogManager.getLogger(Bootstrap.class);
-        LOGGER.info("Starting application");
+        LOGGER.info("Starting tests");
 
         try {
             AppPaths.bootStrap();
         } catch (final IOException e) {
             throw new RuntimeException(e);
         }
+
+        initialized = true;
     }
 }
